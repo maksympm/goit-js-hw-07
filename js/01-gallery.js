033c8ecee5f9207f227ec5526c3e196031e8b58c
Maksym Popov <80689833+maksympm@users.noreply.github.com>
2023-09-03 18:34:07 +0300

    1task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const markup = createMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+galleryContainer.addEventListener("click", handleGalleryClick);
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/>
+      </a>
+      </li>`;
+    })
+    .join("");
+}
+function handleGalleryClick(e) {
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  e.preventDefault();
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800" height="600"/>`
+  );
+  instance.show();
+}
 
 console.log(galleryItems);
